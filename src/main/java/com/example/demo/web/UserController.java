db5eeb1e68b268c989503592f02b06fbfbd94182
adamwaniak <RatajJava972Github>
2018-02-14 14:00:46 +0100

    cleaning code

--- src/main/java/com/example/demo/web/UserController.java
+++ src/main/java/com/example/demo/web/UserController.java
@@ -1,18 +1,16 @@
 package com.example.demo.web;
 
 
-import com.example.demo.model.User;
-import com.example.demo.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+        import com.example.demo.model.User;
+        import com.example.demo.service.UserService;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.web.bind.annotation.RequestMapping;
+        import org.springframework.web.bind.annotation.RequestMethod;
+        import org.springframework.web.bind.annotation.ResponseBody;
+        import org.springframework.web.servlet.ModelAndView;
 
-import java.security.Principal;
+        import java.security.Principal;
 
 @Controller
 @RequestMapping("/user")
@@ -27,7 +25,7 @@ public class UserController {
 
     @RequestMapping(value = {"/","/profile"},method = RequestMethod.GET)
     @ResponseBody
-    public User profile(ModelAndView modelAndView, Principal principal, Authentication authentication){
+    public User profile(ModelAndView modelAndView, Principal principal){
         String userEmail = principal.getName();
         User user = userService.findUserByEmail(userEmail);
         return user;
